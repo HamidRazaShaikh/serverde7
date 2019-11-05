ded8f40dfbe7469bcdae417d2673ed12ed0be881
Hamid raza <hamidraza.chemistry@gmail.com>
2019-11-05 18:06:46 +0500

    commit6

---
+++ routes/schema.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose");
+
+var UserSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    father: {type: String}
+
+});
+
+exports.UserModel = mongoose.model("Users", UserSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,20 @@
 var express = require ('express');
 var user = express.Router();
+var schema = require('./schema');
+var UserModel = schema.UserModel;
 
-var data = [];
+
 
 
 user.get ('/get',function (req,res){
-    res.send({data : data});
+    res.send({data : UserModel});
     console.log(data);
 });
 
 user.post('/add', function (req, res) {
-    data.push(req.body);
+    UserModel.push(req.body);
     console.log(req.body);
-    res.send({status: 200, data: data})
+    res.send({status: 200, data: UserModel})
 });
 
 module.exports = user;

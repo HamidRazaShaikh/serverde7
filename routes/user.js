155c8d40a2aa878eee4a080b3740073802c04f02
Hamid raza <hamidraza.chemistry@gmail.com>
2019-11-06 09:55:20 +0500

    commit8

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,20 @@
-var express = require ('express');
+var express = require('express');
 var user = express.Router();
 var schema = require('./schema');
 var UserModel = schema.UserModel;
 
 
-
-
-user.get ('/get',function (req,res){
-    res.send({data : UserModel});
-    console.log(data);
+user.get('/get', function (req, res) {
+    UserModel.find().exec(function (error, data) {
+        res.send({error: error, data: data})
+    });
 });
 
 user.post('/add', function (req, res) {
-    UserModel.push(req.body);
-    console.log(req.body);
-    res.send({status: 200, data: UserModel})
+    var user = new UserModel(req.body);
+    user.save(function (error, data) {
+        res.send({error: error, data: data})
+    })
 });
 
 module.exports = user;
